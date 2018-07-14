b3979b59a0186f62e905fd196a579855c024ba73
Ryan Tillman-French <39138118+ResilientRyry@users.noreply.github.com>
2018-07-13 21:39:10 -0400

    Create codewarDay15.js

---
+++ codewarDay15.js
@@ -0,0 +1,39 @@
+
+function switchItUp(number){
+  switch (number) {
+    case 1:
+    return "One";
+    break;
+    case 2:
+    return "Two";
+    break;
+    case 3:
+    return "Three";
+    break;
+    case 4:
+    return "Four";
+    break;
+    case 5:
+    return "Five";
+    break;
+    case 6:
+    return "Six";
+    break;
+    case 7:
+    return "Seven";
+    break;
+    case 8:
+    return "Eight";
+    break;
+    case 9:
+    return "Nine";
+    break;
+    case 0:
+    return "Zero";
+    break;
+  }
+}
+
+// When provided with a number between 0-9, return it in words. 
+// Input :: 1 Output :: "One". 
+// Try using "Switch" statements.
