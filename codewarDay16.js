8e53ccd7bfc33ef06198da3324156a8883e089d8
Ryan Tillman-French <39138118+ResilientRyry@users.noreply.github.com>
2018-07-16 18:50:50 -0400

    Create codewarDay16.js

---
+++ codewarDay16.js
@@ -0,0 +1,12 @@
+function minMax(arr){
+return [Math.min(...arr), Math.max(...arr)]
+  
+}
+
+// Story
+// Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, 
+// this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. 
+// Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+// Task
+// Write a function that returns both the minimum and maximum number of the given list/array.
