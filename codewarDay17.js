d05e8ab87da5804196f895699b3c9588a09b762c
Ryan Tillman-French <39138118+ResilientRyry@users.noreply.github.com>
2018-07-17 09:54:45 -0400

    Create codewarDay17.js

---
+++ codewarDay17.js
@@ -0,0 +1,11 @@
+function roundToNext5(n){
+  // ...
+  return Math.ceil(n/5)*5;
+}
+
+// Given an integer as input, can you round it to the next (meaning, "higher") 5?
+
+// Input may be any positive or negative integer (including 0).
+
+// You can assume that all inputs are valid integers.
+
