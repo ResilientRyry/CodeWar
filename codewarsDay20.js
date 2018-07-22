c57e62cb867955802af2d78b51395868b22909c0
Ryan Tillman-French <39138118+ResilientRyry@users.noreply.github.com>
2018-07-22 05:03:44 -0400

    Create codewarsDay20.js

---
+++ codewarsDay20.js
@@ -0,0 +1,10 @@
+function even_or_odd(number) {
+  if (number % 2 == 0){
+    return ("Even")
+  }else {
+    return ("Odd")
+  }
+}
+
+// Create a function that takes an integer as an argument
+ // returns "Even" for even numbers or "Odd" for odd numbers.
