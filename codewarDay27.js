fa8b6e2581270cd795231fbf03bcce9abb2410cc
Ryan Tillman-French <39138118+ResilientRyry@users.noreply.github.com>
2018-08-03 00:56:09 -0400

    Create codewarDay27.js

---
+++ codewarDay27.js
@@ -0,0 +1,20 @@
+// Your task is to write function findSum.
+
+// Upto and including n, this function will return the sum of all multiples of 3 and 5.
+
+// For example:
+
+// findSum(5) should return 8 (3 + 5)
+
+// findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+
+
+function findSum(n) {
+    let sum = 0;
+    for(let i = 1; i <= n; i++){
+      if(i % 3 === 0 || i % 5 === 0){
+        sum +=i
+      }
+    }
+    return sum;
+  }
